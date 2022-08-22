541a33815c98eaf9cb8c2fa4865b23f64f1ea5b6
nguyenhainhat17 <nguyenhainhatx@gmail.com>
2022-08-22 13:57:54 +0700

    update finish

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import Router from "./config/Router";
 import useWindowDimensions from "./hooks/useWindowDimensions";
 import Home from "./pages/Home/Home";
 import Footer from "./components/footer/Footer";
+import { useLocation } from "react-router-dom";
 
 function App() {
   const { height, width } = useWindowDimensions();
+
+  const location = useLocation();
+
+  console.log(location.pathname);
 
   return (
     <>
       <Header />
       {width > 991 && <Menu />}
       <Router />
-      <Footer bottom />
+      {location.pathname === "/detail" ? <Footer bottom /> : <Footer />}
     </>
   );
 }
